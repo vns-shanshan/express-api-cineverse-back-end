38bdad6c1cc875f0a1f5e23436f6a2d419147489
huijingj <vanessajin0426@gmail.com>
2025-11-10 12:32:26 -0800

    Upgrade - Replaced fetch with axios in frontend and fixed the corresponding CORS error in backend

--- server.js
+++ server.js
@@ -15,6 +15,12 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+if (process.env.NODE_ENV !== 'production') {
+  app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }));
+}
 
 // Import routers
 const authRouter = require('./controllers/auth');
@@ -30,7 +36,6 @@ mongoose.connection.on('connected', () => {
 });
 
 // Middleware
-app.use(cors());
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(logger('dev'));
